cd7c4baf11e8d55b7aabc57e87a97fe77b81481a
Andrija Stevanovic <andrija.stevanovic56@gmail.com>
2022-05-23 15:28:28 -0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+#include "../lib/console.h"
+#include "../lib/hw.h"
+
+int main() {
+
+    for (int i = 0; i < 13; i++) {
+        while (true) {
+            break;
+        }
+    }
+    return 0;
+}
